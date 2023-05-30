368f961374e80ebe2f7529cd9293f71a022480d5
Đỗ Trọng Nhân <dotrongnhan15102000@gmail.com>
2023-05-30 10:34:00 +0700

    fix: test api

--- server.js
+++ server.js
@@ -23,9 +23,10 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.use("/", express.static(path.join(__dirname, "public")));
+// app.use("/", express.static(path.join(__dirname, "public")));
 
-app.use("/", require("./route/root"));
+// app.use("/", require("./route/root"));
+app.use('/', (req, res) => res.send('hello bae'));
 // route for users
 app.use("/users", require("./route/userRoutes"));
 // route for ages
@@ -38,29 +39,29 @@ app.use("/skills", require("./route/skillRoutes"));
 app.use("/products", require("./route/productRoutes"));
 
 app.all("*", (req, res) => {
-  res.status(404);
-  if (req.accepts("html")) {
-    res.sendFile(path.join(__dirname, "views", "404.html"));
-  } else if (req.accepts("json")) {
-    res.json({
-      message: "404 Not Found",
-    });
-  } else {
-    res.type("txt").send("404 Not Found");
-  }
+   res.status(404);
+   if (req.accepts("html")) {
+      res.sendFile(path.join(__dirname, "views", "404.html"));
+   } else if (req.accepts("json")) {
+      res.json({
+         message: "404 Not Found",
+      });
+   } else {
+      res.type("txt").send("404 Not Found");
+   }
 });
 
 app.use(errorHandler);
 
 mongoose.connection.once("open", () => {
-  console.log("Connected to mongoDB");
-  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+   console.log("Connected to mongoDB");
+   app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 });
 
 mongoose.connection.on("error", (err) => {
-  console.log(err);
-  logEvents(
-    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
-    "mongoErrLog.log"
-  );
+   console.log(err);
+   logEvents(
+      `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+      "mongoErrLog.log"
+   );
 });
