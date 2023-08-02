efbb6d48df7718dcdcb442bdd2abcc11e2776ffd
dtnhan <dotrongnhan15102000@gmail.com>
2023-08-02 22:08:33 +0700

    fix: add createNewOrder

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -13,6 +13,63 @@ const getOrdersList = asyncHandler(async (req, res) => {
    }
 });
 
+const createNewOrder = asyncHandler(async (req, res) => {
+   try {
+      const {
+         userId,
+         name,
+         gender,
+         phone,
+         address,
+         notes,
+         payment_method,
+         total,
+         productsList,
+      } = req.body;
+      /** items required */
+      const inputArray = [
+         userId,
+         name,
+         gender,
+         phone,
+         address,
+         payment_method,
+         total,
+         productsList,
+      ];
+      /** check required item */
+      if (inputArray.some((value) => !value || value === '')) {
+         return res.status(404).json({ message: 'All fields are required' });
+      }
+
+      const orderObject = {
+         userId: userId,
+         name: name,
+         gender: gender,
+         phone: phone,
+         address: address,
+         notes: notes || '',
+         payment_method: payment_method,
+         total: total,
+         productsList: productsList,
+      };
+
+      const product = await Order.create(orderObject);
+      if (product) {
+         return res.status(201).json({
+            message: `Order successfully`,
+         });
+      } else {
+         return res.status(400).json({
+            message: 'Order failed',
+         });
+      }
+   } catch (error) {
+      return res.status(400).json({ error, message: "Server's error" });
+   }
+});
+
 module.exports = {
    getOrdersList,
+   createNewOrder,
 };
--- route/orderRoutes.js
+++ route/orderRoutes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const ordersController = require('../controllers/ordersController');
 
-router.route('/').get(ordersController.getOrdersList);
+router
+   .route('/')
+   .get(ordersController.getOrdersList)
+   .post(ordersController.createNewOrder);
 
 module.exports = router;
