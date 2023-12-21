4907df27d4f4883692ff841fe362af9e0bb6d4fd
dtnhan <dotrongnhan15102000@gmail.com>
2023-12-21 10:14:50 +0700

    fix: revert code

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const { logger, logEvents } = require('./middleware/logger');
 const errorHandler = require('./middleware/errorHandler');
 // const corsOptions = require("./config/corsOptions");
 const connectDB = require('./config/dbConn.js');
-const tokenHelper = require('./helpers/tokenHelper.js');
 
 const PORT = process.env.PORT || 3500;
 
@@ -54,46 +53,6 @@ app.use('/orders', require('./route/orderRoutes'));
 
 /** route for mails */
 app.use('/mails', require('./route/mailRoutes'));
-
-app.post('/calendar', async (req, res) => {
-   res.set('Content-Type', 'text/plain');
-   res.send(
-      req.query.validationToken ||
-         'EwCAA8l6BAAUAOyDv0l6PcCVu89kmzvqZmkWABkAAQ0e42KEy1IcwnEpn9Wvl4zV291Xd2RZjV7Uk076IwuLqfEUDuimF8Y0WRZzoIHUcDhkmal0rwmCPuNWUOBnCLO/dyfZHrnkIe8yVI/o/DBTU74sVn8pRbDi4vPJQxudfbsIBceSwzZW5wMunQp1mUEt+/lcTNOXDU5BQ1V3LdbwFyZVrl+BFWcqR+5PUfLK5IVo9coDNDCZzj4WsDjUD+ab/qLIsUqbjMic3+etf0M/8DOkOnOYUkRRgv0vm7W42nc7aZl6d94aDlzNJ4K0L1VQ+W8HRmYZkDeqs2q7ANlEst034BD/P+X/IfxajBT+fTQHuR/VJ4Q24SK6E1e4ncQDZgAACE53W4yk8IzqUAJykBGTnHTHI54hXXd8SaDCpdkQ2XI17+/th9vqm1yXd/XD5oTxcLwnMl+JEvvszDGbjp4yGWEUQaWQtN+eCke/cPoe+cLRClzYU+jz7Z9tP0q/QAhFMOE3sfXaDUYIMxTzv1pGguSpeKKjq7JcKbFInPwbM7mGR9Y06g4yAEBDAUrLHlqmGNr6i2q1bBs7GfYCCMlMfAyVotazAxkKKLVDR8FU1c6wnvlXT1t2BCdqh+NVtOJo9oX9aX9cDXqxdD7fFi1F7F40HBVqpSFTvzvLeCuV5h7CJ8Bb2gJZe2gBSnUr6yX+N/3QZUCyNCJNUDrO0iz1erUBFk1PMaewiqurA2dZYJOePA6lmsLgB5BDOs2g46pl2y8vlpSwbPmpYU7gz6Qj78b53k/EqwIRNcmPsCLR9/BaixtNdtLxd8jgUVx3CF2/qfKQKLS3Q0e21TrS7qaC0wb7b6t7A4QDCzLU0hk2Rq+YNdlCDCWo7FOQaGohUnjWSfuX7AwTHlmx1g49Zzjn4Bg7nUFNZDphE1Sj66sp9n3gI+ezoTRJzyrDb1hc+FHGy2XYqIChbv2ntOgqQ9i6zNDIY+KTWRO4ncUw7VzSchCoTBp9WyYLpmvGwvHi5IsVzomCZZjtY4zqsIaj2XpVZvSfxqVM0QLAkEjmfr3BkmBaSRQXnquTW+jN4Gz63YTCoWTuSoz4U3HTFlIXcFuMOdKoBueDbYitOCX54VgMFTlqkJlRbESy8mIerj9+zp6mbz61JV3YLMjhVBl29+3bkXPBI38WTEy/H6cNmAI='
-   );
-   return;
-
-   //  // Check for validation tokens, validate them if present
-   //  let areTokensValid = true;
-   //  if (req.body.validationTokens) {
-   //     const appId = process.env.OAUTH_CLIENT_ID;
-   //     const tenantId = process.env.OAUTH_TENANT_ID;
-   //     const validationResults = await Promise.all(
-   //        req.body.validationTokens.map((token) =>
-   //           tokenHelper.isTokenValid(token, appId, tenantId)
-   //        )
-   //     );
-
-   //     areTokensValid = validationResults.reduce((x, y) => x && y);
-   //  }
-
-   //  if (areTokensValid) {
-   //     if (req.body.value) {
-   //        for (let i = 0; i < req.body.value.length; i++) {
-   //           const notification = req.body.value[i];
-
-   //           // Verify the client state matches the expected value
-   //           if (
-   //              notification.clientState == process.env.SUBSCRIPTION_CLIENT_STATE
-   //           ) {
-   //              console.log({ notification });
-   //           }
-   //        }
-   //     }
-   //  }
-
-   //  res.status(202).end();
-});
 
 app.all('*', (req, res) => {
    res.status(404);
